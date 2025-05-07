ce9d3d62db4e91691a663fbe8f014764986a6d2c
Juan Muniz <juan@leao.com>
2025-05-07 19:09:37 -0300

    Hook de autenticação + possibilitando registro de usuario ao firebase

---
+++ src/hooks/useAuthentication.jsx
@@ -0,0 +1,76 @@
+import { db } from "../firebase/config";
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  signOut,
+} from "firebase/auth";
+
+import { useState, useEffect } from "react";
+
+export const useAuthentication = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  const [cancelled, setCancelled] = useState(false);
+
+  const auth = getAuth();
+
+  function checkIfIsCancelled() {
+    if (cancelled) {
+      return;
+    }
+  }
+
+  const createUser = async (data) => {
+    checkIfIsCancelled();
+    setLoading(true);
+    setError(null);
+
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+
+      await updateProfile(user, {
+        displayName: data.displayName,
+      });
+
+      setLoading(false);
+
+      return user;
+    } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
+
+      let systemErrorMessage;
+
+      const errors = {
+        "auth/weak-password": "A senha deve conter pelo menos 6 caracteres.",
+        "auth/email-already-in-use": "Email já cadastrado.",
+        "auth/invalid-email": "Formato de e-mail inválido.",
+      };
+      
+      systemErrorMessage = errors[error.code] || "Ocorreu um erro, tente novamente.";
+
+      setError(systemErrorMessage)
+    }
+
+    
+  };
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return {
+    auth,
+    createUser,
+    error,
+    loading,
+  };
+};
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,34 +1,43 @@
 import styles from "./Register.module.css";
 
 import { useState, useEffect } from "react";
+import { useAuthentication } from "../../hooks/useAuthentication";
 
 const Register = () => {
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
 
-    const [displayName, setDisplayName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const [error, setError] = useState("");
+  const { createUser, error: authError, loading } = useAuthentication();
 
-    const handleSubmit = (e) => {  
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        setError("")
+    setError("");
 
-        const user = {  
-            displayName,
-            email,
-            password
-        }
+    const user = {
+      displayName,
+      email,
+      password,
+    };
 
-        if(password !== confirmPassword){   
-            setError("As senhas precisam ser iguais!")
-            return
-        }
-
-        console.log(user)
+    if (password !== confirmPassword) {
+      setError("As senhas precisam ser iguais!");
+      return;
     }
 
+    const res = await createUser(user);
+
+    console.log(res);
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
 
   return (
     <div className={styles.register}>
@@ -79,7 +88,12 @@ const Register = () => {
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </label>
-        <button className="btn">Cadastrar</button>
+        {!loading && <button className="btn">Cadastrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
         {error && <p className="error">{error}</p>}
       </form>
     </div>
